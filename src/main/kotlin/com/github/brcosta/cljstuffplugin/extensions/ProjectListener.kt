e37a47c0921e1fc142863c51642b3dd874095325
Bruno Costa <bruno@brcosta.com.br>
2022-01-13 17:38:11 -0300

    WIP classpath linting

---
+++ src/main/kotlin/com/github/brcosta/cljstuffplugin/extensions/ProjectListener.kt
@@ -0,0 +1,24 @@
+package com.github.brcosta.cljstuffplugin.extensions
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManagerListener
+
+
+class ProjectListener : ProjectManagerListener {
+    override fun projectOpened(project: Project) {
+        LOG.debug("Project Opened ${project.name}")
+
+
+
+    }
+
+    override fun projectClosing(project: Project) {
+        LOG.debug("Project Closed ${project.name}")
+
+    }
+
+    companion object {
+        private val LOG: Logger = Logger.getInstance(ProjectListener::class.java)
+    }
+}
