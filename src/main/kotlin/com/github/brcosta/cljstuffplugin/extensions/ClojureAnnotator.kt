ff8f61e367d81877bd596e12b5f73228f38b7d4d
Bruno Costa <bruno@brcosta.com.br>
2022-01-23 22:11:19 -0300

    Fix namespace highlight issues

--- src/main/kotlin/com/github/brcosta/cljstuffplugin/extensions/ClojureAnnotator.kt
+++ src/main/kotlin/com/github/brcosta/cljstuffplugin/extensions/ClojureAnnotator.kt
@@ -6,12 +6,14 @@ import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.util.parentOfType
 import cursive.ClojureLanguage
-import cursive.highlighter.ClojureSyntaxHighlighter
 import cursive.psi.api.ClKeyword
+import cursive.psi.impl.ClSexpComment
 
-@Suppress("unused", "UnstableApiUsage")
+@Suppress("UnstableApiUsage")
 class ClojureAnnotator : Annotator {
+
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         if (element.language != ClojureLanguage.getInstance()) {
             return
@@ -19,20 +21,24 @@ class ClojureAnnotator : Annotator {
 
         try {
             when {
-                element is ClKeyword -> if (element.namespace != null) {
-                    val startOffset = element.textRange.startOffset
-                    if (element.text?.contains("/") == true) {
-                        val prefixRange = TextRange.from(startOffset, element.namespace.length + 1)
-                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-                            .range(prefixRange).textAttributes(ClojureColorsAndFontsPageEx.KEYWORD_NAMESPACE)
-                            .create()
-                    } else {
-                        val range = TextRange.from(startOffset, element.text.length)
-                        val qualifiedName = element.qualifiedName
-                        holder.newAnnotation(HighlightSeverity.INFORMATION, qualifiedName)
-                            .range(range)
-                            .afterEndOfLine()
-                            .tooltip(qualifiedName).create()
+                element is ClKeyword -> {
+                    if (element.namespace != null) {
+                        val startOffset = element.textRange.startOffset
+                        if (isNamespaced(element)) {
+                            val nsLength = element.text.indexOf('/')
+                            val prefixRange = TextRange.from(startOffset, nsLength)
+                            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                                .range(prefixRange).textAttributes(ClojureColorsAndFontsPageEx.KEYWORD_NAMESPACE)
+                                .create()
+                        } else {
+                            val range = TextRange.from(startOffset, element.text.length)
+                            val qualifiedName = element.qualifiedName
+                            holder.newAnnotation(HighlightSeverity.INFORMATION, qualifiedName)
+                                .range(range)
+                                .afterEndOfLine()
+                                .tooltip(qualifiedName)
+                                .create()
+                        }
                     }
                 }
                 isNsReaderMacro(element) -> {
@@ -47,17 +53,23 @@ class ClojureAnnotator : Annotator {
                         .range(range).textAttributes(ClojureColorsAndFontsPageEx.SYMBOL_NAMESPACE)
                         .create()
                 }
-
             }
         } catch (e: Exception) {
             // should not happen
         }
     }
 
+    private fun isNamespaced(element: ClKeyword) =
+        (element.text?.contains("/") == true) &&
+                (element.parentOfType<ClSexpComment>(false) == null)
+
     private fun isNsReaderMacro(element: PsiElement) =
-        element.prevSibling != null && element.prevSibling.text == "#:"
+        (element.prevSibling != null) &&
+                (element.prevSibling.text == "#:")
 
     private fun isReaderNamespacedSymbol(element: PsiElement) =
-        element.nextSibling != null && (element.nextSibling is LeafPsiElement) && (element.nextSibling as LeafPsiElement).elementType.debugName == "ns separator"
-
+        (element.nextSibling != null) &&
+                (element.nextSibling is LeafPsiElement) &&
+                ((element.nextSibling as LeafPsiElement).elementType.debugName == "ns separator") &&
+                (element.parentOfType<ClSexpComment>(false) == null)
 }
