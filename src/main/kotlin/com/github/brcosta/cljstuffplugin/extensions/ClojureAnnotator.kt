c39650eee4112b6593b27f92a3e2c7a98a642518
Bruno Costa <bruno@brcosta.com.br>
2022-01-29 14:48:13 -0300

    Fix highlighting inside comments

--- src/main/kotlin/com/github/brcosta/cljstuffplugin/extensions/ClojureAnnotator.kt
+++ src/main/kotlin/com/github/brcosta/cljstuffplugin/extensions/ClojureAnnotator.kt
@@ -3,7 +3,6 @@ package com.github.brcosta.cljstuffplugin.extensions
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
@@ -74,7 +73,8 @@ class ClojureAnnotator : Annotator {
 
     private fun isNsReaderMacro(element: PsiElement) =
         (element.prevSibling != null) &&
-                (element.prevSibling.text == "#:")
+                (element.prevSibling.text == "#:") &&
+                (element.parentOfType<ClSexpComment>(false) == null)
 
     private fun isReaderNamespacedSymbol(element: PsiElement) =
         (element.nextSibling != null) &&
